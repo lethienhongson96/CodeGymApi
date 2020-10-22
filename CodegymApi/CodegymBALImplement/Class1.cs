17c333432a301baae42ca2d59008864ab384f650
lethienhongson96 <66103162+lethienhongson96@users.noreply.github.com>
2020-10-22 11:33:35 +0700

    HongSon-->Delete class1 in BAL interface and DAL implement

--- CodegymApi/CodegymBALImplement/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CodegymBALImplement
-{
-    public class Class1
-    {
-    }
-}
--- CodegymApi/CodegymBALInterface/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CodegymBALInterface
-{
-    public class Class1
-    {
-    }
-}
